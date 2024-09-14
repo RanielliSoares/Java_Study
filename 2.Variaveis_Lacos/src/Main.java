81f7086cf2081ba270ff9d05674a1e737a93387e
Ranielli Soares <ranielli.pinho@outlook.com>
2024-09-14 01:06:30 -0300

    atribuições de valores e condição IF

--- 2.Variaveis_Lacos/src/Main.java
+++ 2.Variaveis_Lacos/src/Main.java
@@ -4,17 +4,18 @@ public class Main {
         int anoTopGun = 2022;
         double notaTopGun = 8.1;
         boolean incluidoPlano = true;
-
+        String sinopse;
+        sinopse = "A escola naval de pilotos é onde o melhor dos melhores treinam para refinar suas habilidades de voo de elite. Quando o piloto Maverick é enviado para a escola, sua atitude irresponsável e comportamento arrogante o colocam em desacordo com os outros pilotos, especialmente Iceman.";
         System.out.println("Bem vindo ao Screen Match!");
         System.out.println("Filme: Top Gun: Maveric");
         System.out.println("Ano de Lançamento: " + anoTopGun);
         System.out.println("Nota do filme: " + notaTopGun);
+        System.out.println("Sinopse: "+sinopse);
 
-        if (incluidoPlano){
-            System.out.println("Este filme está incluido no plano");
-
-        }else {
+        if (!incluidoPlano) {
             System.out.println("Este filme não está incluido no plano");
+        } else {
+            System.out.println("Este filme está incluido no plano");
         }
 
     }
