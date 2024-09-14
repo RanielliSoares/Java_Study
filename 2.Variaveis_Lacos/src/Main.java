9644bdaa085193aadb0cc1e21110f0e582297d64
Ranielli Soares <ranielli.pinho@outlook.com>
2024-09-14 00:56:11 -0300

    atribuições de valores e condição IF

--- 2.Variaveis_Lacos/src/Main.java
+++ 2.Variaveis_Lacos/src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+
+        int anoTopGun = 2022;
+        double notaTopGun = 8.1;
+        boolean incluidoPlano = true;
+
         System.out.println("Bem vindo ao Screen Match!");
+        System.out.println("Filme: Top Gun: Maveric");
+        System.out.println("Ano de Lançamento: " + anoTopGun);
+        System.out.println("Nota do filme: " + notaTopGun);
+
+        if (incluidoPlano){
+            System.out.println("Este filme está incluido no plano");
+
+        }else {
+            System.out.println("Este filme não está incluido no plano");
+        }
 
     }
 }
